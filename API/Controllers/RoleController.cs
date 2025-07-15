540d93c1fe272641dd19648e38d101a9dc7774b8
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-07-15 19:42:32 +0200

    Create RoleController.cs

---
+++ API/Controllers/RoleController.cs
@@ -0,0 +1,46 @@
+﻿using API.DataAccess.Repositories;
+using API.Models;
+using API.Validation;
+using LanguageExt;
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class RoleController : ControllerBase
+{
+    private readonly RoleRepository _roleRepository;
+    private readonly UserRepository _userRepository;
+    public RoleController(RoleRepository roleRepository, UserRepository userRepository)
+    {
+        _roleRepository = roleRepository;
+        _userRepository = userRepository;
+    }
+
+    [HttpGet("{username}")]
+    [Authorize]
+    public async Task<IActionResult> GetRoleFromName(string username)
+    {
+        Result<User> userResult = await _userRepository.GetUserAsync(username);
+
+        if (!userResult.IsSuccess) return userResult.ToActionResult();
+
+        User user = userResult.Match(
+            Succ: u => u,
+            Fail: _ => throw new ExceptionalException(
+                "An error occurred while retrieving the user. " +
+                "This should never happen.",
+                new NotFoundException("User retrieval failed.")
+            )
+        );
+
+        if (user.Role == null) return NotFound("User does not have a role assigned.");
+
+        Result<Role> roleResult = await _roleRepository.GetFromIdAsync(user.Role.Id);
+        return roleResult.ToActionResult();
+    }
+}
