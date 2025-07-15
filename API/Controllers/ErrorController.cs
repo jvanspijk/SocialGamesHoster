97423a059d540cb05add68fe3a121cf2dcba3297
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-07-15 19:42:28 +0200

    Create ErrorController.cs

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorController : ControllerBase
+{
+    [Route("/Error")]
+    public IActionResult HandleError()
+    {
+        var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+        if (exceptionFeature != null)
+        {
+            var exception = exceptionFeature.Error;
+            // TODO: maybe logging if necessary
+            return Problem(
+                detail: exception.Message,
+                title: "An error occurred.",
+                statusCode: StatusCodes.Status500InternalServerError
+            );
+        }
+        return Problem(
+            detail: "An unexpected error occurred.",
+            title: "Unknown Error",
+            statusCode: StatusCodes.Status500InternalServerError
+        );
+    }
+}
