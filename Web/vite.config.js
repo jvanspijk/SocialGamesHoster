28b0f35a4961833d65ff8f298c0ba572819b4494
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-07-15 19:40:45 +0200

    clean up vite config

--- Web/vite.config.js
+++ Web/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
 import { env } from 'process';
-import tailwindcss from '@tailwindcss/vite'
 
 const baseFolder =
     env.APPDATA !== undefined && env.APPDATA !== ''
@@ -14,17 +13,10 @@ if (!fs.existsSync(baseFolder)) {
     fs.mkdirSync(baseFolder, { recursive: true });
 }
 
-//const target = env.ASPNETCORE_HTTP_PORTS ? `https://localhost:${env.ASPNETCORE_HTTP_PORTS}` :
-//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:32777';
-////const target = 'http://localhost:32678';
-
-const target = process.env.API_URL || 'http://localhost:32678';
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        plugin(),
-        tailwindcss(),
+        plugin()
     ],
     resolve: {
         alias: {
@@ -32,18 +24,6 @@ export default defineConfig({
         }
     },
     server: {
-        // The following is only valid in dev environments, so we need to find another way for production.
-        //proxy: {
-        //    '^/weatherforecast': {
-        //        target,
-        //        secure: false
-        //    },
-        //    '^/user': {
-        //        target,
-        //        secure: false,
-        //        changeOrigin: true
-        //    },            
-        //},
         port: parseInt(env.DEV_SERVER_PORT || '51144'),
         host: '0.0.0.0',
         watch: {
