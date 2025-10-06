3b6099207403f0ad044d8401810489569fb5b042
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-10-06 22:02:02 +0200

    Update AdminEndpoints.cs

--- API/AdminEndpoints.cs
+++ API/AdminEndpoints.cs
@@ -1,5 +1,9 @@
-﻿using API.AdminFeatures.Players.Endpoints;
+﻿using API.AdminFeatures.Abilities.Endpoints;
+using API.AdminFeatures.Abilities.Responses;
+using API.AdminFeatures.Players.Endpoints;
 using API.AdminFeatures.Players.Responses;
+using API.AdminFeatures.Roles.Endpoints;
+using API.AdminFeatures.Roles.Responses;
 
 namespace API;
 
@@ -7,8 +11,40 @@ public static class AdminEndpoints
 {
     public static IEndpointRouteBuilder MapAdminEndpoints(this IEndpointRouteBuilder builder)
     {
+        builder.MapAbilityEndpoints();
         builder.MapPlayerEndpoints();
         return builder;
+    }
+
+    private static RouteGroupBuilder MapAbilityEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var abilityGroup = builder.MapGroup("/abilities")
+            .WithTags("Abilities");
+
+        abilityGroup.MapGet("/", GetAbilities.HandleAsync);
+
+        abilityGroup.MapGet("/{id:int}", GetAbility.HandleAsync)
+            .WithName("GetAbilityById")
+            .Produces<AbilityResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound);
+
+        return abilityGroup;
+    }
+
+    private static RouteGroupBuilder MapRoleEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var abilityGroup = builder.MapGroup("/roles")
+            .WithTags("Roles");
+
+        // TODO:
+        //abilityGroup.MapGet("/", GetRoles.HandleAsync);
+
+        //abilityGroup.MapGet("/{id:int}", GetRole.HandleAsync)
+        //    .WithName("GetAbilityById")
+        //    .Produces<RoleResponse>(StatusCodes.Status200OK)
+        //    .ProducesProblem(StatusCodes.Status404NotFound);
+
+        return abilityGroup;
     }
 
     private static RouteGroupBuilder MapPlayerEndpoints(this IEndpointRouteBuilder builder)
@@ -20,12 +56,12 @@ public static class AdminEndpoints
 
         playerGroup.MapGet("/{id:int}", GetPlayer.HandleByIdAsync)
             .WithName("GetPlayerById")
-            .Produces<PlayerResponse>(StatusCodes.Status200OK)
+            .Produces<FullPlayerResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status404NotFound);
 
         playerGroup.MapGet("/{name:alpha}", GetPlayer.HandleByNameAsync)
             .WithName("GetPlayerByName")
-            .Produces<PlayerResponse>(StatusCodes.Status200OK)
+            .Produces<FullPlayerResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status404NotFound);
 
         return playerGroup;
