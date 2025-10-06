219d582bef32f43677d2ced96f27135361559ab0
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-10-06 21:38:18 +0200

    Update FullPlayerResponse.cs

--- API/Features/Players/Responses/FullPlayerResponse.cs
+++ API/Features/Players/Responses/FullPlayerResponse.cs
@@ -21,15 +21,20 @@ public record struct RoleWithoutAbilities(int Id, string Name, string Descriptio
 public record struct RoleWithAbilities(int Id, string Name, string Description, List<AbilityResponse> Abilities) : IProjectable<Role, RoleWithAbilities>
 {
     public static Expression<Func<Role, RoleWithAbilities>> Projection =>
-    role => new RoleWithAbilities(
-        role.Id,
-        role.Name,
-        role.Description,
-        role.Abilities
-            .AsQueryable()
-            .Select(AbilityResponse.Projection)
-            .ToList()
-    );
+        role => new RoleWithAbilities(
+            role.Id,
+            role.Name,
+            role.Description,
+            role.Abilities
+                .AsQueryable()
+                .Select(AbilityResponse.Projection)
+                .ToList()
+        );
+}
+public record struct PartialPlayerResponse(int Id, string Name, RoleWithoutAbilities? Role) : IProjectable<Player, PartialPlayerResponse>
+{
+    public static Expression<Func<Player, PartialPlayerResponse>> Projection => 
+        player => new PartialPlayerResponse(player.Id, player.Name, new RoleWithoutAbilities(player.Role.Id, player.Role.Name, player.Role.Description));
 }
 public record struct FullPlayerResponse(int Id, string Name, RoleWithAbilities Role, List<RoleWithoutAbilities> CanSee);
 
