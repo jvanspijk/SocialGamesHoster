0bea37c0e0411aa0901ca172a45f8c3a542f14cd
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-10-15 16:02:33 +0200

    fix for getting ruleset

--- API/Features/Rulesets/Endpoints/GetRuleset.cs
+++ API/Features/Rulesets/Endpoints/GetRuleset.cs
@@ -1,16 +1,18 @@
-﻿using API.DataAccess.Repositories;
+﻿using API.DataAccess;
+using API.DataAccess.Repositories;
+using API.Features.Abilities.Responses;
 using API.Features.Rulesets.Responses;
 
 namespace API.Features.Rulesets.Endpoints;
 
 public static class GetRuleset
 {
-    public static async Task<IResult> HandleAsync(RulesetRepository repository, int id)
+    public static async Task<IResult> HandleAsync(RulesetRepository repository, int rulesetId)
     {
-        RulesetResponse? response = await repository.GetAsync<RulesetResponse>(id);
+        RulesetResponse? response = await repository.GetAsync<RulesetResponse>(rulesetId);
         if (response == null)
         {
-            return Results.Problem($"Ruleset with id {id} not found.");
+            return Results.Problem($"Ruleset with id {rulesetId} not found.");
         }
         return Results.Ok(response);
     }
