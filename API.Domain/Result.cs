70dc059c9f8d796ffed96227835575c8b194c14f
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-10-22 11:03:18 +0200

    Add generic result type for void results

--- API.Domain/Result.cs
+++ API.Domain/Result.cs
@@ -3,16 +3,35 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace API.Domain;
 
+public partial record Result
+{
+    public Error? Error { get; init; }
+    [MemberNotNullWhen(false, nameof(Error))]
+    public bool IsSuccess { get; init; }
+    [MemberNotNullWhen(true, nameof(Error))]
+    public bool IsFailure => !IsSuccess;
+
+    protected internal Result(Error? error, bool isSuccess)
+    {
+        Error = error;
+        IsSuccess = isSuccess;
+    }
+
+    public static Result Success() => new(null, true);
+    public static Result Failure(Error error) => new(error, false);
+    public static implicit operator Result(Error error) => Failure(error);
+}
+
 /// <summary>
 /// Result type of <typeparamref name="T"/>, <see cref="Error"/>.
 /// It will contain a <typeparamref name="T"/> Value or <see cref="Error"/> Error, but never both.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public abstract partial record Result<T> where T : notnull
+public abstract partial record Result<T> : Result where T : notnull
 {
+    public new Error? Error { get; init; }
     public T? Value { get; init; }
-    public Error? Error { get; init; }
-    protected Result(T? value, Error? error, bool isSuccess)
+    protected Result(T? value, Error? error, bool isSuccess) : base(error, isSuccess)
     {       
         Value = value;
         Error = error;
@@ -26,10 +45,10 @@ public abstract partial record Result<T> where T : notnull
     /// </summary>
     [MemberNotNullWhen(true, nameof(Value))]
     [MemberNotNullWhen(false, nameof(Error))]
-    public bool IsSuccess { get; init; }
+    public new bool IsSuccess { get; init; }
     [MemberNotNullWhen(false, nameof(Value))]
     [MemberNotNullWhen(true, nameof(Error))]
-    public bool IsFailure => !IsSuccess;
+    public new bool IsFailure => !IsSuccess;
 
     public bool TryGetValue([MaybeNullWhen(false)] out T value)
     {
@@ -41,8 +60,9 @@ public abstract partial record Result<T> where T : notnull
     {
         error = !IsSuccess ? Error : default;
         return !IsSuccess;
-    }
+    }    
 }
+
 
 internal record Success<T>(T Value) : Result<T>(Value, null, Value != null) where T : notnull
 {
