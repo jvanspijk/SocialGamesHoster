506fc3d735f938e049f7caa172e6baa523226b32
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-10-27 21:14:34 +0100

    automatically redirect to lobby

--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome</h1>
 <a href="/lobby">Go to Lobby</a>
