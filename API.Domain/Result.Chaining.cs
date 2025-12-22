18fe184f651ebb7d9787aba0652748adc4c5109a
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2025-12-22 16:54:26 +0100

    Update Result.Chaining.cs

--- API.Domain/Result.Chaining.cs
+++ API.Domain/Result.Chaining.cs
@@ -2,32 +2,20 @@
 using System.Threading.Tasks;
 
 namespace API.Domain;
-
+#pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
 /// <summary>
 /// Chains operations on <see cref="Result{T}"/> types, enabling functional-style composition of success and failure flows.
 /// </summary>
 public static class ResultChainingExtensions
 {
-    /// <summary>
-    /// Chains this result with the next operation, which returns a <see cref="Result{TOut}"/>.
-    /// If this result is successful, applies <paramref name="next"/> to the value.
-    /// Otherwise, propagates the failure.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <typeparam name="TOut"></typeparam>
-    /// <param name="result"></param>
-    /// <param name="next"></param>
-    /// <returns></returns>
-    /// <exception cref="InvalidOperationException"></exception>
-    public static Result<TOut> Then<T, TOut>(this Result<T> result, Func<T, Result<TOut>> next)
+    public static Result<TOut> Bind<T, TOut>(this Result<T> result, Func<T, Result<TOut>> function)
         where T : notnull
         where TOut : notnull
     {
-        return result switch
+        return (result) switch
         {
-            Success<T> success => next(success.Value),
-            Failure<T> failure => new Failure<TOut>(failure.Error),
-            _ => throw new InvalidOperationException("Unrecognized Result type")
+            Failure<T> failure => failure.Error,
+            Success<T> success => function(success.Value)
         };
     }
 
@@ -38,8 +26,7 @@ public static class ResultChainingExtensions
         return result switch
         {
             Success<T> success => new Success<TNew>(mapSuccess(success.Value)),
-            Failure<T> failure => new Failure<TNew>(failure.Error),
-            _ => throw new InvalidOperationException("Unrecognized Result type")
+            Failure<T> failure => new Failure<TNew>(failure.Error)
         };
     }        
 
@@ -49,22 +36,21 @@ public static class ResultChainingExtensions
         return result switch
         {
             Success<T> success => onSuccess(success.Value),
-            Failure<T> failure => onFailure(failure.Error),
-            _ => throw new InvalidOperationException("Unrecognized Result type")
+            Failure<T> failure => onFailure(failure.Error)
         };
     }
 }
 
 public static class ResultAsyncChainingExtensions
 {
-    public static async Task<Result<TOut>> Then<TIn, TOut>(
+    public static async Task<Result<TOut>> Bind<TIn, TOut>(
     this Task<Result<TIn>> sourceTask,
     Func<TIn, Result<TOut>> next)
     where TIn : notnull
     where TOut : notnull
     {
         Result<TIn> sourceResult = await sourceTask;
-        return sourceResult.Then(next);
+        return sourceResult.Bind(next);
     }
 
     public static async Task<Result<TOut>> Map<TIn, TOut>(
@@ -84,12 +70,7 @@ public static class ResultAsyncChainingExtensions
     where T : notnull
     {
         var result = await sourceTask;
-        return result switch
-        {
-            Success<T> success => onSuccess(success.Value),
-            Failure<T> failure => onFailure(failure.Error),
-            _ => throw new InvalidOperationException("Unrecognized Result type")
-        };
+        return result.Match(onSuccess, onFailure);
     }
 
     public static async Task<TOut> MatchAsync<T, TOut>(
@@ -102,34 +83,8 @@ public static class ResultAsyncChainingExtensions
         return await (result switch
         {
             Success<T> success => onSuccess(success.Value),
-            Failure<T> failure => onFailure(failure.Error),
-            _ => throw new InvalidOperationException("Unrecognized Result type")
+            Failure<T> failure => onFailure(failure.Error)
         });
-    }
-
-    public static Task<Result<TOut>> ThenAsync<TIn, TOut>(
-    this Task<Result<TIn>> sourceTask,
-    Func<TIn, Task<Result<TOut>>> next)
-    where TIn : notnull
-    where TOut : notnull
-
-    {
-        return sourceTask.MatchAsync(
-            onSuccess: next,
-            onFailure: error => Task.FromResult<Result<TOut>>(error)
-        );
-    }
-
-    public static Task<Result<TNew>> MapAsync<TIn, TNew>(
-        this Task<Result<TIn>> sourceTask,
-        Func<TIn, Task<TNew>> mapSuccess)
-        where TIn : notnull
-        where TNew : notnull
-    {
-        return sourceTask.ThenAsync<TIn, TNew>(async value =>
-        {
-            TNew newValue = await mapSuccess(value);
-            return new Success<TNew>(newValue);
-        });
-    }    
+    }      
 }
+#pragma warning restore CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
