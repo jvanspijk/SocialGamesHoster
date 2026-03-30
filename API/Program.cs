e9bb496586baa217ba3fceceae56fa5d042c9780
jvanspijk <49561246+jvanspijk@users.noreply.github.com>
2026-03-30 20:47:13 +0200

    add config for admin credentials

--- API/Program.cs
+++ API/Program.cs
@@ -27,12 +27,10 @@ namespace API;
 // - Remove docker entirely. Use install and run script instead.
 // - Uniform error handling. In svelte, add a CreateFail function to ApiError so that we can return after !res.ok something like: return res.error.fail()
 // v1:
-// - Maybe add Open telemetry for performance logging
 // - Fix login for admins
 //      - store admin credentials in database or environment variables
 // - Admin: force logout users, (decouple IP from user)
 // - Change participants in active game sessions
-//   - Remove players from game sessions
 // - Fix login for players
 //      - login using player id (?)
 //      - Use local IP address to identify players
@@ -57,7 +55,7 @@ namespace API;
 // - roles have win conditions
 // - IAbility interface so that a DM can create custom abilities for roles (will be god object)
 //    - Parse abilities from JSON or scripting language instead so that users don't have to write code
-
+// - Maybe add Open telemetry for performance logging
 
 // Optional:
 // - Assign random roles to players
@@ -68,15 +66,37 @@ namespace API;
 // - Streaming database results using IAsyncEnumerable where possible, especially for endpoints that return lists of data. <-- while the asp.net api result can return a stream, it needs to be handled client side too. There's also some latency involved in starting to return results, so it might not be worth it for smaller lists of data. But for larger lists, it can improve performance and reduce memory usage.
 // - Use JSON source generator for serialization where possible (see GetAbility endpoint for example). <-- Does it save enough time? 
 
-// Bugs/issues:
-// - Minor issue: cancelling a round increments the round number (not talking about the id). This might be an issue for games where there's a fixed number of rounds.
-// - Rounds can only be created with a timer that is started at creation and overrides the previous one. There's no way to create a round without a timer.
+// Errors:
+/*
+ * [500] GET /game/me
+Error: Cannot use `cookies.set(...)` after the response has been generated
+    at event2.cookies.set (node_modules\@sveltejs\kit\src\runtime\server\respond.js:708:11)
+    at Object.delete (node_modules\@sveltejs\kit\src\runtime\server\cookie.js:174:12)
+    at invalidate_session (src\lib\tokens.svelte.ts:33:10)
+    at load (src\routes\game\(authenticated)\me\+page.server.ts:22:13)
+*/
 
 public class Program
 {
+    private readonly static string _solutionDirectory = Directory.GetParent(Environment.CurrentDirectory)?.FullName
+        ?? throw new InvalidOperationException("Couldn't determine solution directory.");
     public static void Main(string[] args)
     {
         Console.WriteLine("Booting up app");
+        string configPath = Path.Combine(_solutionDirectory, "config.ini");
+
+        var config = new ConfigurationBuilder()
+            .AddIniFile(configPath, optional: false, reloadOnChange: true)
+            .Build();
+
+        string? adminName = config["Admin:Username"];
+        string? adminPass = config["Admin:Password"];
+
+        if(adminName is null || adminPass is null)
+        {
+            throw new InvalidOperationException("Admin credentials not found in config.ini");
+        }
+
         var builder = WebApplication.CreateBuilder(args);
 
         if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
